b848ffa32fbe9785916a63472ceb8b357652d618
Devid Zanon <d.zanon93@gmail.com>
2021-06-22 17:32:27 +0200

    added .vs and .vscode to gitignore

--- source/Cipher.py
+++ source/Cipher.py
@@ -1,46 +1,42 @@
-#!/usr/bin/env python
-
-# In general, this function should be 'def caesar(plainText, shift):
-# We set a costant shift of 2
 
 import string
 
-class Cipher():
-  
-  def Caesar(self,plainText,shift):
-    """Insert text to encode and shift"""
-    cipherText = "";
-    for char in plainText:
-      if(char.isupper() == True):
-        stayInAlphabet = ord(char) + shift;
-        if(stayInAlphabet > ord('Z')):
-          stayInAlphabet -= 26;
-        finalLetter = chr(stayInAlphabet);
-        cipherText += finalLetter;
-      elif(char.islower() == True):
-          stayInAlphabet = ord(char) + shift;
-          if(stayInAlphabet > ord('z')):
-            stayInAlphabet -= 26;
-          finalLetter = chr(stayInAlphabet);
-          cipherText += finalLetter;
-      elif(char.isdigit() == True):
-            digit = int(char);
-            finalNumber = ((digit + shift) % 10);
-            cipherText = cipherText + str(finalNumber);
-      else:
-          # if the char is nor a letter nor a digit,
-          # just copy it (special characters, etc..)
-          cipherText = cipherText + str(char);
-      return cipherText;
-
-    def ProgressiveCaesar(plainText,shift):
-        l = len(plainText) - 1;
-        cipherText = "";
+class Cipher:
+    
+    def __init__(self, cipherText):
+        self.cipherText = cipherText
+        
+    def Caesar(self,plainText,shift):
+        """Insert text to encode and shift"""
+        cipherText = ""
+        for char in plainText:
+            if(char.isupper() == True):
+                stayInAlphabet = ord(char) + shift
+                if(stayInAlphabet > ord('Z')):
+                    stayInAlphabet -= 26
+                finalLetter = chr(stayInAlphabet)
+                cipherText += finalLetter
+            elif(char.islower() == True):
+                stayInAlphabet = ord(char) + shift
+                if(stayInAlphabet > ord('z')):
+                    stayInAlphabet -= 26
+                finalLetter = chr(stayInAlphabet)
+                cipherText += finalLetter
+            elif(char.isdigit() == True):
+                digit = int(char)
+                finalNumber = ((digit + shift) % 10)
+                cipherText = cipherText + str(finalNumber)
+            else:
+                # if the char is nor a letter nor a digit
+                # just copy it (special characters, spaces, etc.)
+                cipherText = cipherText + str(char)
+            
+            self.cipherText = cipherText
+            
+    def ProgressiveCaesar(self,plainText,shift):
+        l = len(plainText) - 1
+        cipherText = ""
         for i in range(0,l):
-            char = plainText[i];
-            cipherChar = self.Caesar(char,shift + i);
-            cipherText = cipherText + cipherChar;
-        return cipherText;
-
-    def ProgressiveAlternateCaesar(plainText,shift):
-        base_shift = shift;
+            char = plainText[i]
+            cipherChar = self.C
+        
